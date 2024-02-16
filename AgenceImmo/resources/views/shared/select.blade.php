e016889a55d3b81d8d28dc9bb96ab274aba3121c
Lacreation <lynearmandcreation@gmail.com>
2024-02-16 23:18:36 +0100

    [ADD] select input view

---
+++ AgenceImmo/resources/views/shared/select.blade.php
@@ -0,0 +1,20 @@
+@php
+    $name ??= '';
+    $class ??= null;
+    $label ??= ucfirst($name);
+    $value ??= '';
+@endphp
+
+<div @class(["form-group", $class])>
+    <label for="{{ $name }}">{{$label}}</label>
+    <select name="{{$name}}[]" id="{{$name}}" multiple>
+        @foreach ($options as $k => $v)
+            <option @selected($value->contains($k)) value="{{$k}}">{{$v}}</option>
+        @endforeach
+    </select>
+    @error($name)
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
+</div>
