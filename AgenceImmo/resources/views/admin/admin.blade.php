b630f402c2714d73a37d7fac4dc790a7a241bd28
Lacreation <lynearmandcreation@gmail.com>
2024-02-17 23:06:37 +0100

    [UPDATE] add tom select

--- AgenceImmo/resources/views/admin/admin.blade.php
+++ AgenceImmo/resources/views/admin/admin.blade.php
@@ -6,6 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.bootstrap5.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
         <title>@yield('title') | Administration</title>
     </head>
 
@@ -52,5 +54,9 @@
 
             @yield('content')
         </div>
+
+        <script>
+            new TomSelect('select[multiple]', {plugins: {remove_button: {title: 'Supprimer'}}})
+        </script>
     </body>
 </html>
